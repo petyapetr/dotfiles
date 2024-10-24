d91a626b1094d0146ca27351bd3771c60b6c6ba1
Slava Zakharov <90175094+petyapetr@users.noreply.github.com>
2024-10-24 18:01:45 +0400

    Fix wsl ctrl+c hotkey

--- wezterm.lua
+++ wezterm.lua
@@ -26,7 +26,7 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.keys = {
 		{
 			key = "c",
-			mods = "CTRL",
+			mods = "CTRL|SHIFT",
 			action = act.CopyTo "Clipboard"
 		},
 		{
