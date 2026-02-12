e4c5c48d4027dacf203c29f60bcffb0968840372
Slava Zakharov <90175094+petyapetr@users.noreply.github.com>
2026-02-12 13:41:03 +0400

    Update wezterm.lua keybindings

--- wezterm.lua
+++ wezterm.lua
@@ -9,6 +9,11 @@ config.font_size = 18
 
 config.window_decorations = "RESIZE"
 
+local target = wezterm.target_triple
+local is_windows = target:find("windows") ~= nil
+local is_macos = target:find("darwin") ~= nil
+local primary_mod = is_macos and "CMD" or "CTRL"
+
 config.enable_tab_bar = true
 -- config.use_fancy_tab_bar = false
 -- config.show_close_tab_button_in_tabs = true
@@ -28,133 +33,86 @@ config.color_scheme = "Dracula (Official)"
 config.hide_tab_bar_if_only_one_tab = false
 config.window_close_confirmation = "NeverPrompt"
 
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+if is_windows then
 	config.window_decorations = "RESIZE | TITLE"
-	config.keys = {
-		{
-			key = "c",
-			mods = "CTRL|SHIFT",
-			action = act.CopyTo "Clipboard"
-		},
-		{
-			key = "v",
-			mods = "CTRL",
-			action = act.PasteFrom "Clipboard"
-		},
-		{
-			key = "t",
-			mods = "CTRL",
-			action = act.SpawnCommandInNewTab { domain = "DefaultDomain" }
-		},
-		{
-			key = "t",
-			mods = "CTRL|SHIFT",
-			action = act.SpawnTab "CurrentPaneDomain"
-		},
-		{
-			key = ";",
-			mods = "CTRL",
-			action = act.SplitVertical { domain = "CurrentPaneDomain" }
-		},
-		{
-			key = "'",
-			mods = "CTRL",
-			action = act.SplitHorizontal { domain = "CurrentPaneDomain" }
-		},
-		{
-			key = "w",
-			mods = "CTRL",
-			action = act.CloseCurrentPane { confirm = false }
-		},
-		{
-			key = "j",
-			mods = "ALT",
-			action = act.ActivateTabRelative(-1)
-		},
-		{
-			key = "l",
-			mods = "ALT",
-			action = act.ActivateTabRelative(1)
-		},
-		{
-			key = "i",
-			mods = "CTRL|SHIFT",
-			action = act.ActivatePaneDirection "Up"
-		},
-		{
-			key = "k",
-			mods = "CTRL|SHIFT",
-			action = act.ActivatePaneDirection "Down"
-		},
-		{
-			key = "j",
-			mods = "CTRL|SHIFT",
-			action = act.ActivatePaneDirection "Left"
-		},
-		{
-			key = "l",
-			mods = "CTRL|SHIFT",
-			action = act.ActivatePaneDirection "Right"
-		},
-	}
-else
-	config.keys = {
-		{
-			key = "t",
-			mods = "CMD",
-			action = act.SpawnCommandInNewTab { domain = "DefaultDomain" }
-		},
-		{
-			key = "t",
-			mods = "CMD|SHIFT",
-			action = act.SpawnTab "CurrentPaneDomain"
-		},
-		{
-			key = ";",
-			mods = "CMD",
-			action = act.SplitVertical { domain = "CurrentPaneDomain" }
-		},
-		{
-			key = "'",
-			mods = "CMD",
-			action = act.SplitHorizontal { domain = "CurrentPaneDomain" }
-		},
-		{
-			key = "w",
-			mods = "CMD",
-			action = act.CloseCurrentPane { confirm = false }
-		},
-		{
-			key = "j",
-			mods = "CMD",
-			action = act.ActivateTabRelative(-1)
-		},
-		{
-			key = "l",
-			mods = "CMD",
-			action = act.ActivateTabRelative(1)
-		},
-		{
-			key = "i",
-			mods = "CMD|SHIFT",
-			action = act.ActivatePaneDirection "Up"
-		},
-		{
-			key = "k",
-			mods = "CMD|SHIFT",
-			action = act.ActivatePaneDirection "Down"
-		},
-		{
-			key = "j",
-			mods = "CMD|SHIFT",
-			action = act.ActivatePaneDirection "Left"
-		},
-		{
-			key = "l",
-			mods = "CMD|SHIFT",
-			action = act.ActivatePaneDirection "Right"
-		},
-	}
 end
+
+config.keys = {
+	{
+		key = "t",
+		mods = "CTRL|SHIFT",
+		action = act.DisableDefaultAssignment,
+	},
+	{
+		key = "t",
+		mods = "CMD|SHIFT",
+		action = act.DisableDefaultAssignment,
+	},
+	{
+		key = "t",
+		mods = "CMD",
+		action = act.SpawnTab "CurrentPaneDomain",
+	},
+	{
+		key = "t",
+		mods = primary_mod .. "|SHIFT",
+		action = act.SpawnCommandInNewTab { domain = "DefaultDomain", cwd = "~"},
+	},
+	{
+		key = ";",
+		mods = primary_mod,
+		action = act.SplitVertical { domain = "CurrentPaneDomain" },
+	},
+	{
+		key = "'",
+		mods = primary_mod,
+		action = act.SplitHorizontal { domain = "CurrentPaneDomain" },
+	},
+	{
+		key = "w",
+		mods = primary_mod,
+		action = act.CloseCurrentPane { confirm = false },
+	},
+	{
+		key = "j",
+		mods = "ALT",
+		action = act.ActivateTabRelative(-1),
+	},
+	{
+		key = "l",
+		mods = "ALT",
+		action = act.ActivateTabRelative(1),
+	},
+	{
+		key = "UpArrow",
+		mods = primary_mod,
+		action = act.ActivatePaneDirection "Up",
+	},
+	{
+		key = "DownArrow",
+		mods = primary_mod,
+		action = act.ActivatePaneDirection "Down",
+	},
+	{
+		key = "LeftArrow",
+		mods = primary_mod,
+		action = act.ActivatePaneDirection "Left",
+	},
+	{
+		key = "RightArrow",
+		mods = primary_mod,
+		action = act.ActivatePaneDirection "Right",
+	},
+	{
+		key = "c",
+		mods = primary_mod .. "|SHIFT",
+		action = act.CopyTo "Clipboard",
+	},
+	{
+		key = "v",
+		mods = primary_mod,
+		action = act.PasteFrom "Clipboard",
+	},
+}
 
 return config
