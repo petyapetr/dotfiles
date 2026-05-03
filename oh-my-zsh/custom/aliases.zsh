5051afbea2e07a96191990b3876c625259c98b75
Slava Zakharov <90175094+petyapetr@users.noreply.github.com>
2026-05-03 17:04:29 +0400

    Update git aliases

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -5,21 +5,6 @@ alias pn=pnpm
 alias cat="bat --plain"
 
 # git aliases
-alias gst="git status"
-alias gsts="git status -s"
-alias gc="git commit"
-alias amend="git commit --amend --no-edit"
-alias stash="git stash"
-alias ga="git add"
-alias gaa="git add -A"
-alias gd="git diff"
-alias gdc="git diff --cached"
-alias gla="git la"
-alias glr="git lr"
-alias gsw="git switch"
-alias gb="git branch"
-alias gbr="git branch -r"
-alias grb="git rebase"
 
 # other
 alias pnx="pnpm dlx"
